e8e10ddac6a1a464a9973bfac943cb4f08243907
GreyA <grey.altaer@gmail.com>
2025-01-26 14:48:23 -0800

    exercise 4 complete

--- app.js
+++ app.js
@@ -24,3 +24,19 @@ Complete Exercise 2 in the space below:
 foods.push('pizza', 'cheeseburger');
 
 console.log('Exercise 2 result:', foods);
+
+
+/*
+Exercise 4: Access an array element
+
+1) Retrieve the 'pizza' string from the array based on its position (index) in
+   the array.  
+
+2) Assign it to a variable called `favFood`.
+
+Complete Exercise 4 in the space below:
+*/
+
+let favFood = foods[0];
+
+console.log('Exercise 4 result:', favFood);
