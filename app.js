34d58693b606319f24802302ab695703225a4010
GreyA <grey.altaer@gmail.com>
2025-01-26 15:39:47 -0800

    refactor exercise 14 for cleaner syntax

--- app.js
+++ app.js
@@ -229,6 +229,6 @@ Exercise 14: Accessing within nested arrays
 Complete Exercise 14 in the space below:
 */
 
-const num = numArrays[2] [1];
+const num = numArrays[2][1];
 
 console.log('Exercise 14 result:', num);
